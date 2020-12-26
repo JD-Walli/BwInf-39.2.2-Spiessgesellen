45614c76eda3ee196f01460797ac91987e3112c1
JD_Walli <jakov@wazungu.eu>
2020-12-26 15:43:52 +0100

    readData method

---
+++ BwInf 39.2.2 Spießgesellen/BwInf 39.2.2 Spießgesellen/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BwInf_39_2_2_Spießgesellen {
+    class Program {
+        static void Main(string[] args) {
+            var tup = readData(1);
+            List<Spiess> spiesse = tup.Item2;
+            foreach(Spiess spiess in spiesse) {
+                spiess.printSpiess();
+            }
+            Console.ReadLine();
+        }
+
+        public static Tuple<Spiess,List<Spiess>> readData(int number) {
+            string[] lines = System.IO.File.ReadAllLines(System.IO.Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.FullName+"/spiesse"+number+".txt");
+            Spiess wunschspiess = new Spiess(new List<int>(), lines[1].Trim().Split(' ').ToList());
+            List<Spiess> spiesse = new List<Spiess>();
+            for(int i = 3; i < int.Parse(lines[2])*2+3;i+=2) {
+                spiesse.Add(new Spiess(lines[i].Trim().Split(' ').ToList(), lines[i + 1].Trim().Split(' ').ToList()));
+            }
+            return new Tuple<Spiess, List<Spiess>>(wunschspiess, spiesse);
+        }
+    }
+}
---
+++ BwInf 39.2.2 Spießgesellen/BwInf 39.2.2 Spießgesellen/Spiess.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BwInf_39_2_2_Spießgesellen {
+    class Spiess {
+        int length;
+        List<int> bowls= new List<int>();
+        List<string> fruits = new List<string>();
+
+        public Spiess(List<int> bowls, List<string> fruits) {
+            this.bowls = bowls;
+            this.fruits = fruits;
+            length = fruits.Count();
+        }
+
+        public Spiess(List<string> bowls, List<string> fruits) {
+            foreach(string bowl in bowls) {
+                this.bowls.Add(int.Parse(bowl.Trim()));
+            }
+            this.fruits = fruits;
+            length = fruits.Count();
+        }
+
+        public void printSpiess() {
+            string output= "";
+            foreach(string fruit in fruits) {
+                output += fruit + " ";
+            }
+            output += " -> ";
+            foreach(int bowl in bowls) {
+                output += bowl + " ";
+            }
+            Console.WriteLine(output);
+        }
+    }
+}
